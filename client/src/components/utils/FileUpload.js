69dab75214ecae5f39ba28338d7a067065470f98
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-06-23 15:33:50 +0900

    #1.7 Erase Image

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -23,6 +23,15 @@ function FileUpload() {
     });
   };
 
+  const deleteHandler = (image) => {
+    const currentIndex = Images.indexOf(image);
+
+    let newImages = [...Images];
+
+    newImages.splice(currentIndex, 1);
+    setImages(newImages);
+  };
+
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
       <Dropzone onDrop={dropHandler}>
@@ -52,7 +61,7 @@ function FileUpload() {
         }}
       >
         {Images.map((image, index) => (
-          <div key={index}>
+          <div onClick={() => deleteHandler(image)} key={index}>
             <img
               style={{ minWidth: "300px", width: "300px", height: "240px" }}
               src={`http://localhost:5000/${image}`}
