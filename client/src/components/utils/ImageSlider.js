1689c9a0e058d4b9d05a97e5d125af8e1345cba3
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-07-27 19:20:55 +0900

    #2.3 Make Image Slider

---
+++ client/src/components/utils/ImageSlider.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Carousel } from "antd";
+
+function ImageSlider(props) {
+  return (
+    <div>
+      <Carousel autoplay>
+        {props.images.map((image, index) => (
+          <div key={index}>
+            <img
+              style={{ width: "100%", maxHeight: "150px" }}
+              src={`http://localhost:5000/${image}`}
+            />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+}
+
+export default ImageSlider;
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -3,6 +3,7 @@ import { FaCode } from "react-icons/fa";
 import Axios from "axios";
 import { Icon, Col, Card, Row } from "antd";
 import Meta from "antd/lib/card/Meta";
+import ImageSlider from "../../utils/ImageSlider";
 
 function LandingPage() {
   const [Products, setProducts] = useState([]);
@@ -21,14 +22,7 @@ function LandingPage() {
     console.log("product", product);
     return (
       <Col lg={6} md={8} xs={24} key={index}>
-        <Card
-          cover={
-            <img
-              style={{ width: "100%", maxHeight: "150px" }}
-              src={`http://localhost:5000/${product.images[0]}`}
-            />
-          }
-        >
+        <Card cover={<ImageSlider images={product.images} />}>
           <Meta title={product.title} description={`$${product.price}`} />
         </Card>
       </Col>
