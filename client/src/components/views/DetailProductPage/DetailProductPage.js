0e16eb0aa7277da4e0eeee38e15207f136101123
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-08-31 16:00:10 +0900

    #3.2 Create Product Image Component

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,8 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Row, Col } from "antd";
+import ProductImage from "./Sections/ProductImage";
+import ProductInfo from "./Sections/ProductInfo";
 
 function DetailProductPage(props) {
   const productId = props.match.params.productId;
+
+  const [Product, setProduct] = useState({});
 
   useEffect(() => {
     axios
@@ -10,13 +15,33 @@ function DetailProductPage(props) {
       .then((response) => {
         if (response.data.success) {
           console.log("response.data", response.data);
+          setProduct(response.data.product[0]);
         } else {
           alert("Failed to get detail information.");
         }
       });
   }, []);
 
-  return <div>DetailProductPage</div>;
+  return (
+    <div style={{ width: "100%", padding: "3rem 4rem" }}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <h1>{Product.title}</h1>
+      </div>
+
+      <br />
+
+      <Row gutter={[16, 16]}>
+        <Col lg={12} sm={24}>
+          {/* ProductImage */}
+          <ProductImage detail={Product} />
+        </Col>
+        <Col lg={12} sm={24}>
+          {/* ProductInfo */}
+          <ProductInfo />
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default DetailProductPage;
---
+++ client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProductInfo() {
+  return <div>ProductInfo</div>;
+}
+
+export default ProductInfo;
