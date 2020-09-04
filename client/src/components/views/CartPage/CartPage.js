6cb02cd0cfc36acff17b16ac71c4c311167b0842
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-09-04 15:35:38 +0900

    #4.9 Create Paypal Button

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { Empty } from "antd";
 import { getCartItem, removeCartItem } from "../../../_actions/user_actions";
 import UserCardBlock from "./Sections/UserCardBlock";
+import Paypal from "../../utils/Paypal";
 
 function CartPage(props) {
   const dispatch = useDispatch();
@@ -68,6 +69,8 @@ function CartPage(props) {
           <Empty description={false} />
         </>
       )}
+
+      <Paypal />
     </div>
   );
 }
